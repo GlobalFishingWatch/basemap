4d577d50b2697f669f4734e365914b04b145e55c
erik <erik@satellitestud.io>
2020-09-25 12:07:49 +0200

    merged all styles into a single style + ui to pick

--- README.md
+++ README.md
@@ -9,3 +9,12 @@ Usage:
 - generate tiles: `yarn generate`
 - run server: `yarn serve`
 - run viewer: `yarn start`
+
+Source shapefiles and other can be found in this shared Drive folder: https://drive.google.com/drive/u/2/folders/1piSE5s-ooxRBGSSKaWv-VTa6N9V_lZ4w
+
+### Attribution:
+
+- Bathymetry: NASA Visible Earth https://visibleearth.nasa.gov/images/73963/bathymetry
+- Countries/regions: GADM https://gadm.org/data.html
+- Populated places/graticules: Natural Earth https://www.naturalearthdata.com/
+- World Ports Index: https://msi.nga.mil/Publications/WPI
--- scripts/generate.js
+++ scripts/generate.js
@@ -3,74 +3,106 @@ const exec = require('child_process').execSync
 const fs = require('fs')
 const rimraf = require('rimraf')
 
-const VECTOR_TILES_PATH = 'tiles/vector'
 const GENERATE_BATHYMETRY = false
 const CONVERT_TO_GEOJSON = false
-const MAX_ZOOM = 6
+const MAX_ZOOM = 9
 
 // generate bathymetry tiles from merged and styled bathymetry tiff (see qgis project)
 if (GENERATE_BATHYMETRY) {
-  exec('gdal2tiles.py ./data/bathymetry_merged_styled.tif ./tiles/bathymetry --zoom 0-8 --xyz --webviewer=none')
+  exec('gdal2tiles.py ./data/bathymetry_merged_styled.tif ./tiles/basemap_bathymetry --zoom 0-8 --xyz --webviewer=none')
 }
 
 // vector
 let cmd = ''
 
-const layers = [
+const layersSets = [
   {
-    name: 'countries',
-    input: 'data/gadm36_levels_shp/gadm36_0.shp',
-    tippecanoe: '--exclude-all',
-    // skip: true
+    path: 'tiles/basemap_gadm',
+    layers: [
+      // {
+      //   name: 'countries',
+      //   input: 'data/gadm36_levels_shp/gadm36_0.shp',
+      //   tippecanoe: '--exclude-all',
+      //   // skip: true
+      // },
+      {
+        name: 'regions',
+        input: 'data/gadm36_levels_shp/gadm36_1.shp',
+        tippecanoe: '--exclude-all',
+        minzoom: 4
+      },
+    ],
   },
   {
-    name: 'regions',
-    input: 'data/gadm36_levels_shp/gadm36_1.shp',
-    tippecanoe: '--exclude-all',
-    minzoom: 5
-  },
-  {
-    name: 'country_centroids',
-    geojson: 'data/country_centroids.geojson',
-    tippecanoe: '--no-tile-size-limit --base-zoom=2 --include=SHORT_NAME',
-    minzoom: 2
-    // skip: true
-  },
-  {
-    name: 'populated_places',
-    input: 'data/populated-places/populated-places.shp',
-    // TODO filter our by admin level ?
-    tippecanoe: '--no-tile-size-limit --base-zoom=3',
-    minzoom: 3
-    // skip: true
-  },
-  {
-    name: 'graticules',
-    geojson: 'data/graticules-clean.geojson',
-    tippecanoe: '--no-tile-size-limit --base-zoom=0'
-  },
+    path: 'tiles/basemap_default',
+    layers: [
+      // {
+      //   name: 'landmass',
+      //   input: 'data/gadm36_levels_shp/gadm36_0_merged.shp',
+      //   tippecanoe: '--exclude-all',
+      //   skip: true
+      // },
+      {
+        name: 'countries',
+        input: 'data/gadm36_levels_shp/gadm36_0.shp',
+        tippecanoe: '--exclude-all',
+        // skip: true
+      },
+      {
+        name: 'ports',
+        geojson: 'data/WPI.json',
+        tippecanoe: '--no-tile-size-limit --base-zoom=2 --include=PORT_NAME --include=HARBORSIZE',
+        minzoom: 2
+        // skip: true
+      },
+      {
+        name: 'country_centroids',
+        geojson: 'data/country_centroids.geojson',
+        tippecanoe: '--no-tile-size-limit --base-zoom=2 --include=SHORT_NAME',
+        minzoom: 2
+        // skip: true
+      },
+      {
+        name: 'populated_places',
+        input: 'data/populated-places/populated-places.shp',
+        // TODO filter our by admin level ?
+        tippecanoe: '--no-tile-size-limit --base-zoom=3',
+        minzoom: 3
+        // skip: true
+      },
+      {
+        name: 'graticules',
+        geojson: 'data/graticules-clean.geojson',
+        tippecanoe: '--no-tile-size-limit --base-zoom=0'
+      },
+    ]
+  }
 ]
 
-const allmbtiles = layers.map(layer => {
-  const geojson = layer.geojson || `out/${layer.name}.geojson`
-  const mbtiles = layer.mbtiles || `out/${layer.name}.mbtiles`
-  const minzoom = layer.minzoom || 0
-  if (layer.input && CONVERT_TO_GEOJSON && layer.skip !== true) {
-    exec(`yarn mapshaper ${layer.input} -o format=geojson ${geojson}`)
-  }
-  cmd = `tippecanoe --minimum-zoom=${minzoom} --maximum-zoom=${MAX_ZOOM} -o ${mbtiles} ${layer.tippecanoe} ${geojson} --layer=${layer.name} --force`
-  if (layer.skip !== true) {
-    console.log(cmd)
-    exec(cmd)
-  }
-  return mbtiles
+layersSets.forEach((layerSet) => {
+  if (layerSet.skip) return
+  const layers = layerSet.layers
+  const allmbtiles = layers.map(layer => {
+    const geojson = layer.geojson || `out/${layer.name}.geojson`
+    const mbtiles = layer.mbtiles || `out/${layer.name}.mbtiles`
+    const minzoom = layer.minzoom || 0
+    if (layer.input && CONVERT_TO_GEOJSON && layer.skip !== true) {
+      exec(`yarn mapshaper ${layer.input} -o format=geojson ${geojson}`)
+    }
+    cmd = `tippecanoe --minimum-zoom=${minzoom} --maximum-zoom=${MAX_ZOOM} -o ${mbtiles} ${layer.tippecanoe} ${geojson} --layer=${layer.name} --force`
+    if (layer.skip !== true) {
+      console.log(cmd)
+      exec(cmd)
+    }
+    return mbtiles
+  })
+  
+  rimraf.sync(layerSet.path)
+  fs.mkdirSync(layerSet.path)
+  
+  const mergeCmd = `tile-join ${allmbtiles.join(' ')} --no-tile-size-limit --output-to-directory=${layerSet.path}`
+  console.log(mergeCmd)
+  exec(mergeCmd)
+  
+  // exec('gsutil cp -r tiles/** gs://public-tiles/basemap')
 })
-
-rimraf.sync(VECTOR_TILES_PATH)
-fs.mkdirSync(VECTOR_TILES_PATH)
-
-const mergeCmd = `tile-join ${allmbtiles.join(' ')} --no-tile-size-limit --output-to-directory=${VECTOR_TILES_PATH}`
-console.log(mergeCmd)
-exec(mergeCmd)
-
-exec('gsutil cp -r tiles/** gs://public-tiles/basemap')
